983bc47525b373b2f0fd79ab10314544fae5bbd1
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-09-28 20:48:39 -0300

    chore: set api token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std;
+
 fn main() {
-    println!("Hello, world!");
+    let api_token =
+        std::env::var("API_TOKEN").expect("expected there to be an API_TOKEN environment variable");
+    dbg!(api_token);
 }
