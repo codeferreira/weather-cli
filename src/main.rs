0a8123fdf4a45733e1cddfb3ae38599166244541
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-04 13:04:25 -0300

    feat: add requester for weather api

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use reqwest;
 use std;
 
 fn main() {
     let api_token =
         std::env::var("API_TOKEN").expect("expected there to be an API_TOKEN environment variable");
-    dbg!(api_token);
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    let args: String = arg_iterator.collect();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .send()
+        .expect("a successful request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    dbg!(response);
 }
